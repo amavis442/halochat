a5c4ab52a45c6dfefdd205d5fab4fd84cc152aaf
Patrick Teunissen <p.teunissen18@gmail.com>
2022-12-05 12:55:58 +0100

    Added packages and configs

---
+++ tailwind.config.cjs
@@ -0,0 +1,21 @@
+import('tailwindcss').Config
+module.exports = {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,svelte}",
+    ],
+    theme: {
+        extend: {},
+        colors: {
+            transparent: "transparent",
+            black: "#0f0f0e",
+            white: "#FFFCF2",
+            accent: "#EB5E28",
+            light: "#CCC5B9",
+            medium: "#403D39",
+            dark: "#252422",
+            danger: "#ff0000",
+        },
+    },
+    plugins: [],
+}
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import * as path from 'path'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, 'src'),
+    }
+  },
+  plugins: [svelte(),
+    nodePolyfills({
+      // Whether to polyfill `node:` protocol imports.
+      protocolImports: true,
+    }),
+  ],
 })
