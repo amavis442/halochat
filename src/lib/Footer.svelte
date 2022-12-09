782aeba89b3c08a0467fbb56c194e8e0b6fd508d
Patrick Teunissen <p.teunissen18@gmail.com>
2022-12-09 23:35:12 +0100

    Fix link

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,6 +1,6 @@
 <footer class="footer h-fit">
   <span>Built with Svelte 3 original design by &nbsp;</span>
-  <a href="https://axelfuhrmann.com/" target="_blank" rel="noopener">
+  <a href="https://banpagi.com/" target="_blank" rel="noreferrer">
     Patrick Teunissen
   </a>
 </footer>
