c81875582a00e3eea397e00a0be38ac822792a00
Patrick Teunissen <p.teunissen18@gmail.com>
2022-12-12 19:21:14 +0100

    removed unused store

--- src/lib/stores/eventdata.ts
+++
@@ -1,8 +0,0 @@
-import { writable } from 'svelte/store'
-import { getLocalJson, setLocalJson } from '../util/misc'
-
-export const eventdata = writable(getLocalJson("halonostr/data") || [])
-
-eventdata.subscribe($eventdata => {
-    setLocalJson("halonostr/data", $eventdata)
-})
