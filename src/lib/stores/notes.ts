618ecfec61636a7d823e266398e797fc5cf90e05
Patrick Teunissen <p.teunissen18@gmail.com>
2022-12-12 23:28:27 +0100

    Fix order

--- src/lib/stores/notes.ts
+++ src/lib/stores/notes.ts
@@ -1,17 +1,29 @@
 import { writable, get } from 'svelte/store'
 import { queue } from '../state/app'
 import type { Note } from '../state/types'
-import { setLocalJson,getLocalJson } from '../util/storage'
+import { setLocalJson, getLocalJson } from '../util/storage'
+import { uniqBy, prop, sortBy, head } from "ramda";
 
 export const notes = writable(getLocalJson('halonostr/notes') || [])
 
 export const noteReplyPubKeys = []
+export let since = 0
 
 export function updateNotes(note: Note) {
     const q = get(queue)
+    let $notes = get(notes)
+    if (!$notes.length) {
+        notes.set([])
+    }
+
     notes.update(data => {
-        if (data.find((item) => item.id == note.id)) {
-            return data
+        try {
+            if (data.length && data.find((item) => item.id == note.id)) {
+                return data
+            }
+        } catch (error) {
+            console.log('Error: ', error, data, note)
+            throw error
         }
 
         let tags = note.tags.find(item => item[0] == 'e' && item[3] == 'reply')
@@ -22,11 +34,17 @@ export function updateNotes(note: Note) {
                 q.push(tags[1])
             }
         }
-        data.push(note)
+        data.unshift(note)
         return data
     })
+    
+    $notes = sortBy(prop('created_at'), $notes)
+    let headNote = head($notes)
+    if (headNote) {
+        since = headNote.created_at
+    }    
 }
 
 notes.subscribe((value) => {
     setLocalJson('halonostr/notes', value)
-  })
+})
