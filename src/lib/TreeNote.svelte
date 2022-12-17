f3b18fa3a0ea7c65fc2339864606f7b634b08181
Patrick Teunissen <p.teunissen18@gmail.com>
2022-12-17 20:33:05 +0100

    Css fix

--- src/lib/TreeNote.svelte
+++ src/lib/TreeNote.svelte
@@ -1,40 +1,50 @@
 <script lang="ts">
   import type { Note as NoteEvent } from "./state/types";
   import Note from "./Note.svelte";
-  import { onMount } from "svelte";
+  import { beforeUpdate, onMount } from "svelte";
 
   export let userHasAccount: boolean = false;
   export let notes: Array<NoteEvent>;
   export let expanded: boolean = false;
   export let num: number = 0;
-  export let level:number = 1;
+  export let level: number = 1;
   function toggle() {
     expanded = !expanded;
   }
 
-  let color  = 100
-  onMount(()=>{
-    level = level + 1
-    color = color + level * 100
-  })
+  let color = 100;
+  let border = "";
+  onMount(() => {});
+  beforeUpdate(() => {
+    level = level + 1;
+    color = color + level * 100;
+    border = "border-l-4 border-l-indigo-" + color + "/100";
+  });
 </script>
 
-<button class:expanded on:click={toggle}>
-  {#if expanded}
-    Hide {num} repl{#if num == 1}y{:else}ies{/if}
-  {:else}
-    Show {num} repl{#if num == 1}y{:else}ies{/if}
-  {/if}
-</button>
+<div class="flex items-top gap-4 p-4 ml-16">
+  <button class="text-blue-600 visited:text-purple-600" on:click={toggle}>
+    {#if expanded}
+      Hide {num} repl{#if num == 1}y{:else}ies{/if}
+    {:else}
+      Show {num} repl{#if num == 1}y{:else}ies{/if}
+    {/if}
+  </button>
+</div>
 
 {#if expanded}
-  <ul class="border-l-4 border-l-indigo-{color}/100">
+  <ul class={border}>
     {#each notes ? notes : [] as note (note.id)}
       <li>
         {#if note.replies.length > 0}
-        <Note {note} {userHasAccount} />
+          <Note {note} {userHasAccount} />
 
-          <svelte:self notes={note.replies} {userHasAccount} num={note.replies.length} level={level} />
+          <svelte:self
+            notes={note.replies}
+            {userHasAccount}
+            num={note.replies.length}
+            {level}
+          />
         {:else}
           <Note {note} {userHasAccount} />
         {/if}
