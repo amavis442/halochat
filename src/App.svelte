377892c898c3781d85c7e2acc3e6c3981303de5f
Patrick Teunissen <p.teunissen18@gmail.com>
2022-12-21 15:59:23 +0100

    Account info to top

--- src/App.svelte
+++ src/App.svelte
@@ -8,23 +8,24 @@
   import "@fortawesome/fontawesome-free/css/solid.css";
   import { account } from "./lib/stores/account";
   import Anchor from "./lib/partials/Anchor.svelte";
-  import Toasts from './lib/Toasts.svelte'
-  import {notes} from './lib/stores/notes'
-  import {users} from './lib/stores/users'
-  import {relays} from './lib/state/pool'
-  import { followlist,blocklist } from './lib/state/app'
+  import AccountInfo from "./lib/partials/AccountInfo.svelte";
+
+  import Toasts from "./lib/Toasts.svelte";
+  import { notes } from "./lib/stores/notes";
+  import { users } from "./lib/stores/users";
+  import { relays } from "./lib/state/pool";
+  import { followlist, blocklist } from "./lib/state/app";
   export let url = "";
 
   /* big reset for testing and debugging */
-  if (import.meta.env.VITE_APP_MODE == 'debug') {
-    account.set(null)
-    notes.set(null)
-    relays.set(null)
-    users.set(null)
-    followlist.set(null)
-    blocklist.set(null)
+  if (import.meta.env.VITE_APP_MODE == "debug") {
+    account.set(null);
+    notes.set(null);
+    relays.set(null);
+    users.set(null);
+    followlist.set(null);
+    blocklist.set(null);
   }
-  
 </script>
 
 <Toasts />
@@ -39,6 +40,7 @@
         class="mt-6 grid-cols-1 justify-start items-center pl-4 w-full
         border-gray-600 border-b space-y-3 pb-5"
       >
+        <AccountInfo />
         <nav>
           <p class="nav-p">
             <Link to="/">Feed</Link>
@@ -46,7 +48,7 @@
           <p class="nav-p">
             <Link to="/followed">Follow</Link>
           </p>
-          
+
           <p class="nav-p">
             <Link to="relays">Relays</Link>
           </p>
@@ -57,53 +59,15 @@
             <Link to="account">Account</Link>
           </p>
         </nav>
-
-        <div class="flex items-top justify-center ">
-          <div
-            class="w-full max-w-lg mx-auto bg-white rounded-lg shadow-xl from-blue-100 via-blue-300 to-blue-500 bg-gradient-to-br p-2"
-          >
-            {#if $account && $account.pubkey}
-              <div class="text-center">
-                <span
-                  class="border-4 border-white rounded-full mx-auto inline-block"
-                >
-                  <img
-                    src={$account.picture
-                      ? $account.picture
-                      : "profile-placeholder.png"}
-                    class="w-12 h-12 rounded-full"
-                    alt={$account.name.slice(0, 10)}
-                  />
-                </span>
-              </div>
-
-              <p class="text-center">
-                <span class="font-bold">{$account.name}</span>
-              </p>
-
-              <p class="text-xs text-center mb-5">
-                Pubkey: {$account.pubkey.slice(0, 4)}...{$account.pubkey.slice(
-                  -4
-                )}
-              </p>
-
-              <p class="text-xs text-center mb-5">
-                About: {$account.about}
-              </p>
-            {:else}
-              <span>Not logged in. Use <Anchor href="account">account</Anchor> to create account</span>
-            {/if}
-          </div>
-        </div>
       </div>
     </header>
 
     <main class="grid-cols-2 text-center justify-items-center max-h-max">
       <Route path="/">
-        <Feed isFollowedView={false}/>
+        <Feed isFollowedView={false} />
       </Route>
       <Route path="/followed">
-        <Feed isFollowedView={true}/>
+        <Feed isFollowedView={true} />
       </Route>
       <Route path="relays">
         <Relays />
---
+++ src/lib/partials/AccountInfo.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import { account } from "../stores/account";
+  import Anchor from "./Anchor.svelte";
+</script>
+
+<div class="flex items-top justify-center ">
+  <div
+    class="w-full max-w-lg mx-auto bg-white rounded-lg shadow-xl from-blue-100 via-blue-300 to-blue-500 bg-gradient-to-br p-2"
+  >
+    {#if $account && $account.pubkey}
+      <div class="text-center">
+        <span class="border-4 border-white rounded-full mx-auto inline-block">
+          <img
+            src={$account.picture
+              ? $account.picture
+              : "profile-placeholder.png"}
+            class="w-12 h-12 rounded-full"
+            alt={$account.name.slice(0, 10)}
+          />
+        </span>
+      </div>
+
+      <p class="text-center">
+        <span class="font-bold">{$account.name}</span>
+      </p>
+
+      <p class="text-xs text-center mb-5">
+        Pubkey: {$account.pubkey.slice(0, 4)}...{$account.pubkey.slice(-4)}
+      </p>
+
+      <p class="text-xs text-center mb-5">
+        About: {$account.about}
+      </p>
+    {:else}
+      <span
+        >Not logged in. Use <Anchor href="account">account</Anchor> to create account</span
+      >
+    {/if}
+  </div>
+</div>
