023c670d0b92fc439b5d04b2c83496e8e75dc769
Patrick Teunissen <p.teunissen18@gmail.com>
2023-01-23 23:42:23 +0100

    Nav refactor

--- src/App.svelte
+++ src/App.svelte
@@ -13,7 +13,6 @@
   import { Modals, closeModal } from "svelte-modals";
   import { users } from "./lib/stores/users";
   import Posts from "./lib/Posts.svelte";
-  import { notifications } from "./lib/state/app";
 
   export let url = "";
   
@@ -22,62 +21,51 @@
 
 <Toasts />
 
-<Router {url}>
+<Router url="{url}">
   <div class="flex h-screen w-screen m-auto justify-center">
     <header
       class="mt-6 items-center pl-4 border-gray-600 border-b space-y-3 pb-5 xl:w-2/12 md:w-3/12 sm:w-full"
     >
       <AccountInfo />
       <nav>
-        <p class="nav-p">
+        <p class="nav-p {url === '/' ? 'selected' : ''}">
           <Link to="/" title="Global feed" class="flex"
             >
             <div class="justify-end w-full">
-            Global 
+            Global
           </div>
           </Link>
         </p>
-        <p class="nav-p">
-          <Link to="/followfeed" title="Contacts that you follow"
-            >Following</Link
-          >
+        <p class="nav-p {url === '/followfeed' ? 'selected' : ''}">
+          <Link to="/followfeed" title="Contacts that you follow">
+            Following
+            </Link>
         </p>
-        <p class="nav-p">
+        <p class="nav-p {url === '/posts' ? 'selected' : ''}">
           <Link to="posts" title="Posts you made">Posts</Link>
         </p>
-        <p class="nav-p">
+        <p class="nav-p {url === '/' ? 'selected' : ''}">
           <Link to="contacts" title="Admin list of contacts to follow"
             >Contacts</Link
           >
         </p>
-        <p class="nav-p">
+        <p class="nav-p {url === '/relays' ? 'selected' : ''}">
           <Link to="relays" title="Relays to use">Relays</Link>
         </p>
-        <p class="nav-p">
+        <p class="nav-p {url === '/account' ? 'selected' : ''}">
           <Link to="account" title="Your account data">Account</Link>
         </p>
       </nav>
     </header>
 
     <main class="xl:w-6/12 md:w-9/12 sm:w-full overflow-y-auto">
-      <Route path="/">
-        <Feed />
-      </Route>
-      <Route path="/followfeed">
-        <FollowFeed />
-      </Route>
-      <Route path="/posts">
-        <Posts />
-      </Route>
-      <Route path="relays">
-        <Relays />
-      </Route>
-      <Route path="contacts">
-        <Contacts />
-      </Route>
-      <Route path="account">
-        <Account />
-      </Route>
+      <Route path="/" component="{Feed}" />
+      <Route path="/global" component="{Feed}" />
+      <Route path="/followfeed" component="{FollowFeed}" />
+      <Route path="/posts" component="{Posts}" />
+      <Route path="relays" component="{Relays}" />
+      <Route path="contacts" component="{Contacts}" />
+      <Route path="account" component="{Account}" />
     </main>
   </div>
 </Router>
@@ -100,4 +88,9 @@
     left: 0;
     background: rgba(0, 0, 0, 0.5);
   }
+
+  .selected {
+    text-decoration-line: underline;
+  }
+
 </style>
